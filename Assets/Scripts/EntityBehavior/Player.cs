bba8e7951dfd7256b2d2d119f3899c773a69efdc
iFairPlay22 <ewen.bouquet@gmail.com>
2021-05-31 18:55:11 +0200

    Player animations

--- Assets/Scripts/EntityBehavior/Player.cs
+++ Assets/Scripts/EntityBehavior/Player.cs
@@ -4,70 +4,159 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    private Rigidbody _Rigidbody;
-
-    private Transform _Transform;
-
-    private float lastDirection;
+    /**
+     * Unity variables
+    **/
 
     [Header("Movement")]
-    [SerializeField]
-    private float _TranslationSpeed;
+    [SerializeField] private float _TranslationSpeed;
 
-    [SerializeField]
-    private float _JumpSpeed;
-    [SerializeField]
-    private bool isGrounded;
+    [SerializeField] private float _JumpSpeed;
+    [SerializeField] private bool isGrounded;
+
+    /**
+     * Component variables
+    **/
+    private Rigidbody _Rigidbody;
+    private Transform _Transform;
+    private Animator _animator;
+
+    /**
+     * Local variables
+    **/
+    private float lastDirection;
     private Vector3 velocityChange;
     private int jumpCollision;
+    private bool isJumping;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        // Store the component variables
         _Transform = GetComponent<Transform>();
         _Rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
+
+        // Init local variables
         lastDirection = 1;
     }
 
     private void FixedUpdate()
     {
-        if (isGrounded)
-        {
-            Vector3 newJumpVelocity = _Transform.up * _JumpSpeed * Input.GetAxis("Jump");
-            _Rigidbody.AddForce(newJumpVelocity, ForceMode.Impulse);
-        }
+        // Store user inputs
+        float jumpInput = Input.GetAxis("Jump");
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        float directionSign = Mathf.Sign(Input.GetAxis("Horizontal"));
-        if (Input.GetAxis("Horizontal") != 0 && directionSign != lastDirection)
+        // Move the Player
+        Vector3 newVelocity = _Transform.forward * _TranslationSpeed * horizontalInput;
+        velocityChange = newVelocity - _Rigidbody.velocity;
+        velocityChange.y = 0;
+
+        // Rotate the Player
+        float directionSign = Mathf.Sign(horizontalInput);
+        if (horizontalInput != 0 && directionSign != lastDirection)
         {
             _Transform.GetChild(0).rotation *= Quaternion.Euler(0, 180, 0);
             lastDirection = directionSign;
         }
-        
-        Vector3 newVelocity = _Transform.forward * _TranslationSpeed * Input.GetAxis("Horizontal");
-        velocityChange = newVelocity - _Rigidbody.velocity;
-        velocityChange.y = 0;
-        if (jumpCollision == -1 && Mathf.Sign(velocityChange.x)==-1)
+
+        // Jump the Player
+        if (isGrounded && jumpInput != 0)
+        {
+            Vector3 newJumpVelocity = _Transform.up * _JumpSpeed * jumpInput;
+            _Rigidbody.AddForce(newJumpVelocity, ForceMode.Impulse);
+        }
+
+        // Jump collisions
+        if ((jumpCollision == -1 && Mathf.Sign(velocityChange.x) == -1) || (jumpCollision == 1 && Mathf.Sign(velocityChange.x) == 1))
         {
             velocityChange.x = 0;
         }
-        if (jumpCollision == 1 && Mathf.Sign(velocityChange.x) == 1)
-        {
-            velocityChange.x = 0;
-        }
+
+        // Add movement forces
         _Rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-        
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void Update()
+    {
+        // Animations
+        UpdateAnimations();
+    }
+
+    private void UpdateAnimations()
+    {
+        // Store user inputs
+        float jumpInput = Input.GetAxis("Jump");
+        float fire1Input = Input.GetAxis("Fire1");
+        float fire2Input = Input.GetAxis("Fire2");
+        float fire3Input = Input.GetAxis("Fire3");
+
+        //  - Update Speed to raise left / right animations
+        float xSpeed = Mathf.Abs(_Rigidbody.velocity.x);
+        _animator.SetFloat("Speed", xSpeed);
+
+        //  - Update IsJumping to raise / stop a jump animation
+        if (isGrounded)
+        {
+            // End of a jump
+            if (isJumping)
+            {
+                isJumping = false;
+                _animator.SetBool("IsJumping", isJumping);
+            }
+
+            // Start of a jump
+            if (jumpInput != 0)
+            {
+                isJumping = true;
+                _animator.SetBool("IsJumping", isJumping);
+            }
+        }
+
+        //  - Update IsAttacking to raise an attack animation
+        if (fire1Input != 0)
+        {
+            _animator.SetBool("IsAttacking", true);
+            ExecuteAfterTime(0.1f, () => _animator.SetBool("IsAttacking", false));
+        }
+
+        //  - Update IsAttackingBig to raise a big attack animation
+        if (fire2Input != 0)
+        {
+            _animator.SetBool("IsAttackingBig", true);
+            ExecuteAfterTime(0.1f, () => _animator.SetBool("IsAttackingBig", false));
+        }
+
+        //  - Update IsBlocking to raise a defense animation
+        if (fire3Input != 0)
+        {
+            _animator.SetBool("IsBlocking", true);
+            ExecuteAfterTime(0.1f, () => _animator.SetBool("IsBlocking", false));
+        }
+    }
+
+    private delegate void VoidCallback();
+
+    // Execute some code after time
+    private void ExecuteAfterTime(float time, VoidCallback callback)
+    {
+        StartCoroutine(_ExecuteAfterTime(time, callback));
+    }
+
+    private IEnumerator _ExecuteAfterTime(float time, VoidCallback callback)
+    {
+        yield return new WaitForSeconds(time);
+        callback();
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         float direction = collision.transform.position.x - transform.position.x;
-        if (collision.gameObject.transform.parent.CompareTag("Ground") && collision.gameObject.transform.position.y < _Transform.position.y )
+        if (collision.gameObject.transform.parent.CompareTag("Ground") && collision.gameObject.transform.position.y < _Transform.position.y)
         {
             isGrounded = true;
         }
-        if(collision.gameObject.transform.parent.CompareTag("Wall") && !velocityChange.Equals(Vector3.zero))
+        if (collision.gameObject.transform.parent.CompareTag("Wall") && !velocityChange.Equals(Vector3.zero))
         {
             //Debug.Log("enter");
             if (direction > 0)
@@ -90,11 +179,12 @@ public class Player : MonoBehaviour
             else
                 jumpCollision = -1;
         }
-        if(!isGrounded && stay == false) {
-            if (jumpCollision == -1) { transform.position += new Vector3(0.1f,0,0); }
+        if (!isGrounded && stay == false)
+        {
+            if (jumpCollision == -1) { transform.position += new Vector3(0.1f, 0, 0); }
             if (jumpCollision == 1) { transform.position += new Vector3(-0.1f, 0, 0); }
             jumpCollision = 0;
-            //Debug.Log("jumpCollision 0"); 
+            //Debug.Log("jumpCollision 0");
         }
         if (collision.gameObject.transform.parent.CompareTag("Ground") && collision.gameObject.transform.position.y < _Transform.position.y)
         {
@@ -102,15 +192,16 @@ public class Player : MonoBehaviour
         }
     }
 
-    void OnCollisionExit(Collision collision)
+    private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.transform.parent.CompareTag("Ground"))
         {
             isGrounded = false;
         }
-        if (collision.gameObject.transform.parent.CompareTag("Wall")) { 
-            jumpCollision = 0; 
-            //Debug.Log("exit"); 
+        if (collision.gameObject.transform.parent.CompareTag("Wall"))
+        {
+            jumpCollision = 0;
+            //Debug.Log("exit");
         }
     }
 }
